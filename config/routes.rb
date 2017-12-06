ef23063e4e3ee95be71d2168a0d727d624f06148
Pranathi <neethu@codingdojo.com>
2017-12-06 14:31:30 -0800

    2nd commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ Rails.application.routes.draw do
   get "users" => "users#index"
   get "users/new" => "users#new"
   post "users" => "users#create"  
-  
-  get "users/:id" => "users#show"
+  get "users/total" => "users#total"
 
+  get "users/:id" => "users#show"
   get "users/:id/edit" => "users#edit"
   patch "users/:id" => "users#update"
-  get "users/total" => "users#total"
+  # get "users/total" => "users#total"
   
   
 
